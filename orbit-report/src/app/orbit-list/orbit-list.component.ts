d68a2f7cf3787d9ab440387245a057baf1c5059e
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-09-21 15:29:46 -0500

    test

--- orbit-report/src/app/orbit-list/orbit-list.component.ts
+++ orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -6,10 +6,28 @@ import { Satellite } from '../satellite';
   styleUrls: ['./orbit-list.component.css']
 })
 export class OrbitListComponent implements OnInit {
+  highlight: boolean = true
+
   @Input() satellites: Satellite[];
+  
   constructor() { }
 
-  ngOnInit(): void {
+ngOnInit() {
   }
 
+  sort(column: string): void {
+    this.satellites.sort(function (a: Satellite, b: Satellite): number {
+      if (a[column] < b[column]){
+        return -1;
+      } else if (a[column] > b[column]) {
+        return 1;
+      }
+      return 0;
+    });
+ }
+
+     alternateHighlight(): boolean {
+       this.highlight = !this.highlight;
+       return this.highlight;
+      }
 }
--- orbit-report/src/app/satellite.spec.ts
+++ orbit-report/src/app/satellite.spec.ts
@@ -1,7 +1,8 @@
+  
 import { Satellite } from './satellite';
 
 describe('Satellite', () => {
   it('should create an instance', () => {
-    expect(new Satellite()).toBeTruthy();
+    expect(new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true)).toBeTruthy();
   });
 });
