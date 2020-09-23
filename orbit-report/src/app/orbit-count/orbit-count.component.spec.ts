56af959e2e147c3a68719c6ecba9b7213f020786
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-09-22 19:18:40 -0500

    she works

--- orbit-report/src/app/orbit-count/orbit-count.component.spec.ts
+++ orbit-report/src/app/orbit-count/orbit-count.component.spec.ts
@@ -1,25 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { OrbitCountComponent } from './orbit-count.component';
 
+
+
 describe('OrbitCountComponent', () => {
-  let component: OrbitCountComponent;
-  let fixture: ComponentFixture<OrbitCountComponent>;
+      let component: OrbitCountComponent;
+      let fixture: ComponentFixture<OrbitCountComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ OrbitCountComponent ]
-    })
-    .compileComponents();
-  });
+      
+      beforeEach(async(() => {
+        TestBed.configureTestingModule({
+          declarations: [ OrbitCountComponent ]
+        })
+        .compileComponents();
+}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrbitCountComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+      
+      beforeEach(() => {
+        fixture = TestBed.createComponent(OrbitCountComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
 });
+
+      
+      it('should create', () => {
+        expect(component).toBeTruthy();
+});
+    });
--- orbit-report/src/app/orbit-count/orbit-count.component.ts
+++ orbit-report/src/app/orbit-count/orbit-count.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Satellite } from '../satellite';
+
 
 @Component({
-  selector: 'app-orbit-count',
-  templateUrl: './orbit-count.component.html',
-  styleUrls: ['./orbit-count.component.css']
+
+    selector: 'app-orbit-count',
+    templateUrl: './orbit-count.component.html',
+    styleUrls: ['./orbit-count.component.css']
+
 })
+
 export class OrbitCountComponent implements OnInit {
 
-  constructor() { }
+    @Input() types: { "type": string, "count": number }[];
+    @Input() satellites: Satellite[];
 
-  ngOnInit(): void {
+constructor() { } 
+
+
+ngOnInit() {
   }
 
 }
